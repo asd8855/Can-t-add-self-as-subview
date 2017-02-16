60409fdee092a823669ad115c0e3aec9a56555df
asd <asd13148866@sina.com>
2017-02-16 16:37:01 +0800

    Can't add self as subview bug

---
+++ UINavigationController+Consistent.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Consistent.h
+//  test
+//
+//  Created by libo on 2017/2/16.
+//  Copyright © 2017年 libo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (Consistent)
+
+@end
